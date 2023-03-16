e4c44f8a1e7b17b69b07d874258abb8312cede0d
Adi Setiawan <MGF22120215@kokola.local>
2023-03-16 16:00:21 +0700

    first commit

---
+++ application/config/mongodb.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['host'] = 'localhost';
+$config['port'] = 27017;
+$config['username'] = '';
+$config['password'] = '';
+$config['authenticate'] = FALSE;
